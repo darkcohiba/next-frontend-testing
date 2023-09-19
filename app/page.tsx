38dd302c965d6ffafef5256700b7186d24ecd1b8
Sam Waters <samuel.waters@gmail.com>
2023-09-19 07:26:37 -0600

    added tut link

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import UserButton from './components/UserButton'
 
-
+// following tutorial https://www.youtube.com/watch?v=ZVnjOPwW4ZA
 export default function Home() {
 
   return (
