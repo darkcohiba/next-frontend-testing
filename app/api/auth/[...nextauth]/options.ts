575a3e61aff169355bfbe955492e943122c3787d
Sam Waters <samuel.waters@gmail.com>
2023-09-24 15:29:01 -0600

    next: options

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,1 +1,11 @@
+// https://next-auth.js.org/configuration/options
+
 import type { NextAuthOptions } from "next-auth";
+
+export const options: NextAuthOptions = {
+    providers: [],
+    pages: {
+        signIn: '/signin',
+        signOut: '/signout'
+    }
+}
