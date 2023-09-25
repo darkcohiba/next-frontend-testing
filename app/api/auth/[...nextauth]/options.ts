eeec4f8df1fa6816c0dba93ff9a11a12918ce719
Sam Waters <samuel.waters@gmail.com>
2023-09-24 19:56:30 -0600

    next auth: removed reddit, adding google and credientials

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,14 +1,51 @@
 // https://next-auth.js.org/configuration/options
 
 import type { NextAuthOptions } from "next-auth";
-import RedditProvider from "next-auth/providers/reddit";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials"
+
 
 
 export const options: NextAuthOptions = {
     providers: [
-        RedditProvider({
-            clientId: process.env.REDDIT_CLIENT_ID,
-            clientSecret: process.env.REDDIT_CLIENT_SECRET
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
+        }),
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // You need to provide your own logic here that takes the credentials
+                // submitted and returns either a object representing a user or value
+                // that is false/null if the credentials are invalid.
+                // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+                // You can also use the `req` object to obtain additional parameters
+                // (i.e., the request IP address)
+                const res = await fetch("/your/endpoint", {
+                    method: 'POST',
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" }
+                })
+                const user = await res.json()
+
+                // If no error and we have user data, return it
+                if (res.ok && user) {
+                    return user
+                }
+                // Return null if user data could not be retrieved
+                return null
+            }
         })
     ],
     pages: {
