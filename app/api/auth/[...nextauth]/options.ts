09c8b6170b6440ba5f3d19d1bb668a931e1dbeb3
Sam Waters <samuel.waters@gmail.com>
2023-09-24 21:02:04 -0600

    next auth: finished credentials options

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -22,8 +22,8 @@ export const options: NextAuthOptions = {
         CredentialsProvider({
             name: 'Credentials',
             credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" }
+                username: { label: "Username", type: "text", placeholder: "username" },
+                password: { label: "Password", type: "password", placeholder: "password" }
             },
             async authorize(credentials, req) {
                 // You need to provide your own logic here that takes the credentials
@@ -32,19 +32,31 @@ export const options: NextAuthOptions = {
                 // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
                 // You can also use the `req` object to obtain additional parameters
                 // (i.e., the request IP address)
-                const res = await fetch("/your/endpoint", {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: { "Content-Type": "application/json" }
-                })
-                const user = await res.json()
+                // const res = await fetch("/your/endpoint", {
+                //     method: 'POST',
+                //     body: JSON.stringify(credentials),
+                //     headers: { "Content-Type": "application/json" }
+                // })
+                // const user = await res.json()
 
-                // If no error and we have user data, return it
-                if (res.ok && user) {
-                    return user
+                // // If no error and we have user data, return it
+                // if (res.ok && user) {
+                //     return user
+                // }
+                // // Return null if user data could not be retrieved
+                // return null
+
+                const user = {
+                    id: 1,
+                    username: "cohiba",
+                    password: "123"
                 }
-                // Return null if user data could not be retrieved
-                return null
+
+                if (credentials?.username === user.username && credentials?.password === user.password){
+                    return user
+                }else{
+                    return null
+                }
             }
         })
     ],
