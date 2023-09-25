144ad61525cf7154576c34ae90648302fce37cb3
Sam Waters <samuel.waters@gmail.com>
2023-09-24 18:57:09 -0600

    next auth: reddit

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,16 @@
 // https://next-auth.js.org/configuration/options
 
 import type { NextAuthOptions } from "next-auth";
+import RedditProvider from "next-auth/providers/reddit";
+
 
 export const options: NextAuthOptions = {
-    providers: [],
+    providers: [
+        RedditProvider({
+            clientId: process.env.REDDIT_CLIENT_ID,
+            clientSecret: process.env.REDDIT_CLIENT_SECRET
+        })
+    ],
     pages: {
         signIn: '/signin',
         signOut: '/signout'
